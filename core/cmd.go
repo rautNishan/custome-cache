5d502fde4bc6c39447a0ab8e195f99187fe45762
rautNishan <nishan.raut1709@gmail.com>
2026-01-18 17:08:17 +0545

    Storage init

--- core/cmd.go
+++ core/cmd.go
@@ -3,6 +3,7 @@ package core
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/rautNishan/custome-cache/protocol"
@@ -29,18 +30,19 @@ func (cmd *Command) EvaluateCmdAndResponde(fd int) {
 		cmd.evalPing(writer)
 	case "INFO":
 		cmd.evalOK(writer)
+	case "SET":
+		cmd.evalSet(writer)
+	case "GET":
+	case "TTL":
 	default:
-		cmd.evalError(writer)
+		cmd.evalError("unsupported RESP encode type", writer)
 	}
 }
 
 func (cmd *Command) evalPing(w Writer) {
 	if len(cmd.Args) > 1 {
-		err := errors.New("ERR unknown command")
-		p := protocol.Encode(err, false)
-		w.Write(p)
+		cmd.evalError("incorrect arguments in ping", w)
 	} else if len(cmd.Args) == 1 {
-		fmt.Println("Yes one", cmd.Args[0])
 		p := protocol.Encode(cmd.Args[0], true)
 		w.Write(p)
 
@@ -49,13 +51,53 @@ func (cmd *Command) evalPing(w Writer) {
 		w.Write(p)
 	}
 }
-func (cmd *Command) evalError(w Writer) {
-	err := errors.New("unsupported RESP encode type")
-	p := protocol.Encode(err, false)
-	w.Write(p)
-}
 
 func (cmd *Command) evalOK(w Writer) {
 	p := protocol.Encode("OK", true)
 	w.Write(p)
+}
+
+func (cmd *Command) evalSet(w Writer) {
+	if len(cmd.Args) <= 1 {
+		cmd.evalError("bad arguments in set", w)
+		return
+	}
+	var ttlMs int64 = -1 //there was never ttl
+	key, val := cmd.Args[0], cmd.Args[1]
+	fmt.Println("This is key", key, "and this is val: ", val)
+
+	//right now only supported ttl
+	for i := 2; i < len(cmd.Args); i++ {
+		ex := cmd.Args[i]
+		switch ex {
+		case "EX", "ex":
+			i++
+			//set k v ttl (but no value in ttl)
+			if i == len(cmd.Args) {
+				cmd.evalError("bad arguments", w)
+				return
+			}
+			ttlSec, err := strconv.ParseInt(cmd.Args[i], 10, 64)
+			if err != nil {
+				cmd.evalError("should be an integer", w)
+			}
+			ttlMs = ttlSec * 1000
+		default:
+			cmd.evalError("bad args", w)
+			return
+		}
+
+	}
+	Put(key, NewEntry(val, ttlMs))
+	cmd.evalOK(w)
+}
+
+func (cmd *Command) evalError(msg string, w Writer) {
+	err := getErrorMessage(msg)
+	p := protocol.Encode(err, false)
+	w.Write(p)
+}
+
+func getErrorMessage(msg string) error {
+	return errors.New(msg)
 }
---
+++ core/sotrage.go
@@ -0,0 +1,35 @@
+package core
+
+import (
+	"fmt"
+	"time"
+)
+
+var storage map[string]*Entry
+
+type Entry struct {
+	value     interface{}
+	expiresAt int64 // milliseconds
+}
+
+func init() {
+	storage = make(map[string]*Entry)
+}
+
+func NewEntry(value interface{}, ttlMs int64) *Entry {
+	var defaultExp int64 = -1 //never expires
+
+	if ttlMs > 0 {
+		defaultExp = time.Now().UnixMilli() + ttlMs
+	}
+
+	return &Entry{
+		value:     value,
+		expiresAt: defaultExp,
+	}
+}
+
+func Put(k string, val *Entry) {
+	storage[k] = val
+	fmt.Println(storage[k])
+}
